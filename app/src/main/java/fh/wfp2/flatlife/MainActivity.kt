48b7ce07ab01f53adf164c8e2e9048eaed3f747e
Vincent Schauer <schauervincent@gmail.com>
2020-11-07 22:06:43 +0100

    Initial commit

---
+++ app/src/main/java/fh/wfp2/flatlife/MainActivity.kt
@@ -0,0 +1,12 @@
+package fh.wfp2.flatlife
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
